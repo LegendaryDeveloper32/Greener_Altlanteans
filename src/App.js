6bedf1c97c0de582c33a6b70b1bac5f8f6360c7e
LegendaryDeveloper32 <legendarydeveloper32@outlook.com>
2022-08-25 06:31:06 +0900

    build

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import { Routes, Route, Link } from "react-router-dom";
+//import components
 //import components
 import AOC from "./components/AOC/AOC";
 function App() {
   return (
-    <>
+    <div className="App">
       <AOC />
-    </>
+    </div>
   );
 }
 
